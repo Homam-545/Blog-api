98f812b0d0334929dff61515a49090145b2c09c3
Homam-545 <homam.h2020@gmail.com>
2026-09-07 13:04:20 +0330

    add orgin to settings

--- BlogSystem/settings.py
+++ BlogSystem/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "api-mxn-blogapi.runflare.cloud",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
